c03906dcfcd23d60d9f92761b8fb0249b8878ede
dmytroshurko <dmytrosh1@gmail.com>
2020-08-05 17:43:07 +0300

    Climbing Stairs (K steps)

---
+++ dynamic_programming/climbing_stairs.cpp
@@ -0,0 +1,30 @@
+// You are climbing a stair case. It takes n steps to reach to the top.
+//
+// Each time you can climb 1...k steps. In how many distinct ways can you climb to the top?
+
+#include <iostream>
+#include <vector>
+
+// Time Complexity: O(n * k)
+// Space Complexity: O(k)
+long ClimbStairsKSteps(int n, int k) {
+  std::vector<long> dp(k);
+  dp[0] = 1;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j < k; ++j) {
+      if (i - j < 0) {
+        continue;
+      }
+      dp[i % k] += dp[(i - j) % k];
+    }
+  }
+  return dp[n % k];
+}
+
+int main() {
+  int n, k;
+  std::cin >> n >> k;
+  long result = ClimbStairsKSteps(n, k);
+  std::cout << result << std::endl;
+  return 0;
+}
